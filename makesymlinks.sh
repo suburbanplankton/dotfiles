11b54ee30bcbf2e5985b3687d46f80fb9792d458
Gary Dom <suburbanplankton@gmail.com>
2014-07-01 13:15:20 -0700

    201407011315

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #######################################
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from
+# the home directory to any desired 
+# dotfiles in ~/dotfiles
 #######################################
 
 dir=~/dotfiles                    # dotfiles directory
