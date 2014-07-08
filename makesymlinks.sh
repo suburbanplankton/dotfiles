60f6a9870539931b2af36e743275e3635c3f4f84
Gary Dom <suburbanplankton@gmail.com>
2014-07-08 15:09:49 -0700

    201407081509

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #######################################
 # This script creates symlinks from
-# the home directory to any desired 
+# the home directory to any desired
 # dotfiles in ~/dotfiles
 #######################################
 
@@ -9,41 +9,32 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 
 # list of files to symlink in home directory
-files="bash_profile bashrc display gitconfig minttyrc profile vimrc" # gvimrc
+files="bash_profile bashrc display gitconfig minttyrc profile vimrc vim" # gvimrc
 
 # list of folders to symlink in home directory
-folders="vim"
+#folders="vim"
 
 ##########
 
 # create dotfiles_old directory in home directory if it does not already exist
 if [ ! -d $olddir ]; then
     echo "Creating $olddir for backup of any existing dotfiles in ~"
-	echo
+        echo
     mkdir -p $olddir
 fi
-echo 
+echo
 
 # move any existing files/folders in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    if [ -f ~/.$file ]; then
-        mv ~/.$file ~/dotfiles_old/
-		ln -s $dir/$file ~/.$file
-                echo "Moving ~/.$file to $olddir, and creating symlink to $dir/$file."
-	else
-                ln -s $dir/$file ~/.$file
-		echo "Creating symlink ~/.$file to $dir/$file."
-    fi
+    echo "file $file"
+        if [ -L ~/.$file ]; then
+            rm -f ~/.$file
+            echo "Symbolic link ~/.$file already exists; deleting link."
+        elif [ -f ~/.$file ]; then
+            mv ~/.$file ~/dotfiles_old/
+            echo "Moving ~/.$file to $olddir."
+        fi
+        echo "Creating symbolic link ~/.$file to $dir/$file."
+        ln -s $(realpath $dir)/$file ~/.$file
+    echo 
 done
-
-for folder in $folders; do
-    if [ -f ~/.$file ]; then
-        mv ~/.$file ~/dotfiles_old/
-		ln -s $(realpath dir)/$file ~/.$file
-                echo "Moving ~/.$file to $olddir, and creating symlink to $dir/$file."
-	else
-                ln -s $(realpath dir)/$file ~/.$file
-		echo "Creating symlink ~/.$file to $dir/$file."
-    fi
-done
-
